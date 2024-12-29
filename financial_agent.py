004bc5dce170b69d4ed7bd0ce770530074db6286
Swapnil Jikar <112884653+WizKnight@users.noreply.github.com>
2024-12-30 01:22:07 +0530

    Update financial_agent.py

---
+++ financial_agent.py
@@ -0,0 +1,45 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+Groq.api_key=os.getenv("GROQ_API_KEY")
+
+
+## Web Search Agent
+websearch_agent = Agent(
+    name="Web Search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tools_calls=True,
+    markdown=True,
+)
+
+
+## Financial Agent
+finance_agent = Agent(
+    name="Finance AI Agent",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_news=True)],
+    show_tool_calls=True,
+    description="You are an investment analyst that researches stock prices, analyst recommendations, and stock fundamentals.",
+    instructions=["Format your response using markdown and use tables to display data where possible."],
+    markdown=True
+)
+
+
+multi_ai_agent=Agent(
+    team=[websearch_agent,finance_agent],
+    model=Groq(id="llama-3.1-70b-versatile"),
+    instructions=["Always include sources", "Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent.print_response("Summarize analyst recommendations and share the latest news for NVDA", stream=True)
